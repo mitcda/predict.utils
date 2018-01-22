b7601446a531c2e896d8a9661e85101ba30e807c
= <=>
2018-01-22 12:20:59 +1100

    Added S3 methods for ts, xts objects to splice_series

--- R/predict.utils.R
+++ R/predict.utils.R
@@ -1,32 +1,147 @@
 ### Splice multiple series
 #' @name splice_series
-#' @title Splice two of more vectors into a single series
-#' @description Function to iteratively splice two vectors, covering different bases, into series  extend 
-#' @export
-#' @param x Original (base) vector 
-#' @param y New or future vector series to be applied to the base vector (\code{x})
-#' @param base.period Base period (vector index) on which to splice the two series.  If NULL, then function replaces all NA values in \code{x} using values in \code{y}. (Currently unimplemented.)
-#' @param method One of either 'vector' (default) or 'iterative'.
-#' @param direction One of either 'forward' (default) or 'backward'. (Currently not implemented.)
+#' @title Splice together two time series objects.
+#' @description Function to splice two vectors, covering different, but overlapping periods, into a single consistent time series.
+#' @param x Original (base) time series.
+#' @param y Splicing time series, to be applied to the base time series.
+#' @param base.period Integer object comprising the base period observation. See \link{Details} for more detail explanation about the appropriate format of \code{base.period}. 
+#' @param direction One of either 'after' (default) or 'before'.
 #' @return An object of the same class as object \code{x}. 
-#' @author Dave Mitchell <david.p.mitchell@@homemail.com.au>
-#' @note Arguments \code{base.period} and \code{direction} are currently unimplemented.
+#' @export
+#' @author David Mitchell <david.p.mitchell@@homemail.com.au>
+#' @details The function splices together two time series objects, by applying indexed movements in series \code{y} to base series \code{x}.  Both objects must be of the same frequency (i.e. annual, quarterly, monthly, etc.).  The function also allows for splicing 'forward' (Default)---i.e. extending an existing series beyond the base period---and 'backward'---i.e. splicing values onto the start of a series using the  
+#'
+#' For plain (non-time series) vectors, \code{base.period} must be a two-element integer vector identifying the base period observation index for \code{x} and \code{y}, respectively.  For time series (\code{ts}) objects, \code{base.period} , must be a two element vector specifying the year and year-part (e.g. \code{c(1994, 2)} for the second quarter 1994.  For extensible time series objects (\code{xts}), \code{base.period} must be a time-based class---one of ‘Date’, ‘POSIXct’, ‘timeDate’, ‘yearmon’ or ‘yearqtr’, and match the time-based index class of series \code{x}, and be within the range of \code{x} and \code{y}.
+#' 
+#' @seealso Other R implementations to splice time series include: \code{\link[tframe]{splice}}
 #' @examples
-#'   A <- ts(c(100, 95, 125, 150,  NA,  NA,  NA,  NA), start=1991);
-#'   B <- ts(c(NA,  NA,  NA, 100, 120, 150, 200, 225), start=1991);
-#'   splice_series(A, B);
-splice_series <- function(x, y, base.period=NULL, method="vector", direction="forward")
-{
-  if (method == "iterative") {
-    for (i in seq_along(x)[-1]) 
-      x[i] <- ifelse(!is.na(x[i]), x[i], x[i-1] * y[i] / y[i-1]);
-  }
-  if (method == "vector") {
-    idx <- is.na(x) %>% which
-    x[idx] <- x[min(idx)-1] * y[idx] / y[min(idx)-1];
-  }
-  return(x)
+#'
+#'   ## Non-time series (default) method example
+#'   A <- c(100, 95, 125, 150,  NA,  NA,  NA,  NA);
+#'   B <- c(NA,  NA,  NA, 100, 120, 150, 200, 225);
+#'   splice_series(A, B, base.period=c(4,4));
+#'   splice_series(B, A, base.period=c(4,4), direction="before");
+#'
+#'   ## Time series (ts) object method example
+#'   A <- ts(c(100, 95, 125, 150,  NA,  NA,  NA,  NA), start=1991, frequency=1);
+#'   B <- ts(c(NA,  NA,  NA, 100, 120, 150, 200, 225), start=1991, frequency=1);
+#'   splice_series(A, B, base.period=c(1994,1));
+#'   splice_series(B, A, base.period=c(1994,1), direction="before");
+#'
+#'   ## Extensible time series (xts) object method example
+#'   A <- xts(x=c(100, 95, 125, 150,  NA,  NA,  NA,  NA),
+#'            order.by=seq(as.Date("1991-03-01"), by="quarter", length.out=8));
+#'   B <- xts(x=c(NA,  NA,  NA, 100, 120, 150, 200, 225),
+#'            order.by=seq(as.Date("1991-03-01"), by="quarter", length.out=8));
+#'   splice_series(A, B, base.period=as.Date("1994-03-01"));
+#'   splice_series(B, A, base.period=as.Date("1994-03-01"), direction="before");
+#' 
+splice_series <- function(x, ...) {
+  UseMethod("splice_series")
 }
+
+
+splice_series.default <- function(x, y, base.period, direction="after")
+{
+  ## if ( !class(x) %in% c("numeric","integer") )
+  ##   stop(sprintf("%s should be of type integer or numeric", substitute(x)));
+  if ( !class(y) %in% c("numeric","integer") )
+    stop(sprintf("%s should be of type integer or numeric", deparse(substitute(y))));
+  if ( length(base.period) != 2 )
+    stop("base.period should be of length 2");
+  if ( !class(base.period) %in% c("numeric", "integer") )
+    stop("base.period should be of type integer");
+    if ( !base.period[1] %in% seq_along(x) ) 
+    stop(sprintf("First element of base.period outside %s index range.", deparse(substitute(x))));
+  if ( !base.period[2] %in% seq_along(y) ) 
+    stop(sprintf("First element of base.period outside %s index range.", deparse(substitute(y))));
+  ## Return time series base.period indexes 
+  idx.x <- base.period[1]
+  idx.y <- base.period[2]
+  ## Splice xts time series
+  if (direction == "after")
+    z <- c(x[1:idx.x], x[idx.x] * y[(idx.y+1):length(y)] / y[idx.y])
+  if (direction == "before")
+    z <- c(x[idx.x] * y[1:(idx.y-1)] / y[idx.y], x[idx.x:length(x)])
+  return(z)
+}
+
+
+splice_series.ts <- function(x, y, base.period, direction="after")
+{
+  if ( class(y) != "ts" )
+    stop(sprintf("%s should be of class ts", deparse(substitute(y))));
+  if ( length(base.period) != 2 )
+    stop("base.period should be of length 2");
+  if ( !class(base.period) %in% c("numeric", "integer") )
+    stop("base.period should be of type integer");
+  if ( frequency(x) != frequency(y))
+    stop(sprintf("Objects %s and %s of different frequencies. base.period should be of type integer",
+                 deparse(substitute(x)), deparse(substitute(y))));
+  if ( !round(base.period[1] + (base.period[2]-1)/frequency(x),3) %in% time(x) ) 
+    stop(sprintf("%s time range does not span base.period", deparse(substitute(x))));
+  if ( !round(base.period[1] + (base.period[2]-1)/frequency(y),3) %in% time(y) ) 
+    stop(sprintf("%s time range does not span base.period", deparse(substitute(x))));
+  ## Return time series base.period indexes 
+  idx.x <- which(round(base.period[1] + (base.period[2]-1)/frequency(x),3) == time(x))
+  idx.y <- which(round(base.period[1] + (base.period[2]-1)/frequency(y),3) == time(y))
+  ## Splice xts time series
+  if (direction == "after")
+    z <- ts(splice_series.default(c(x), c(y), base.period=c(idx.x, idx.y), direction=direction),
+            start=start(x), frequency=frequency(x))
+  if (direction == "before")
+    z <- ts(splice_series.default(c(x), c(y), base.period=c(idx.x, idx.y), direction=direction),
+            end=end(x), frequency=frequency(x));
+  return(z)
+}
+
+
+splice_series.xts <- function(x, y, base.period, direction="after")
+{
+  if ( class(y)[1] != "xts" )
+    stop(sprintf("%s should be of class xts", deparse(substitute(y))));
+  if ( class(index(x)) != class(index(y)) )
+    stop(sprintf("Object time indexes of different classes (%s, %s)---should be of same class",
+                 class(index(x)), class(index(y))));
+  if ( !class(base.period) %in% c("Date","POSIXct","timeDate","yearmon","yearqtr") )
+    stop("base.period should be a time-based class (see documentation)");
+  if ( class(base.period) != class(index(x)) )
+    stop(sprintf("base.period not same time-based class as time index of x (%s)---should be of same class",
+                 class(index(x))));
+  if ( !base.period %in% index(x) ) 
+    stop(sprintf("%s time range does not span base.period", deparse(substitute(x))));
+  if ( !base.period %in% index(y) ) 
+    stop(sprintf("%s time range does not span base.period", deparse(substitute(y))));
+  ## Return time series base.period indexes 
+  idx.x <- which(base.period == index(x))
+  idx.y <- which(base.period == index(y))
+  ## Splice xts time series
+  if (direction == "after")
+    z <- xts(splice_series.default(c(coredata(x)), c(coredata(y)),
+                                   base.period=c(idx.x, idx.y), direction=direction),
+             order.by=c(index(x)[1:idx.x], index(y)[-(1:idx.y)]))
+  if (direction == "before")
+    z <- xts(splice_series.default(c(coredata(x)), c(coredata(y)),
+                                   base.period=c(idx.x, idx.y), direction=direction),
+             order.by=c(index(y)[1:(idx.y-1)], index(x)[-(1:(idx.x-1))]))
+  return(z)
+}
+
+
+## #' @param method One of either 'vector' (default) or 'iterative' (NOT IMPLEMENTED).
+## splice_series.original <- function(x, y, base.period=NULL, method="vector", direction="forward")
+## {
+##   if (method == "iterative") {
+##     for (i in seq_along(x)[-1]) 
+##       x[i] <- ifelse(!is.na(x[i]), x[i], x[i-1] * y[i] / y[i-1]);
+##   }
+##   if (method == "vector") {
+##     idx <- is.na(x) %>% which
+##     x[idx] <- x[min(idx)-1] * y[idx] / y[min(idx)-1];
+##   }
+##   return(x)
+## }
+
 
 
 ### Iterative predict function
@@ -39,7 +154,7 @@ splice_series <- function(x, y, base.period=NULL, method="vector", direction="fo
 #' @param ... additional arguments applying to the predictions.
 #' @return The form of the value returned by predict depends on the class of its argument. See the documentation of the particular methods for details of what is produced by that method.
 #' @details This function provides a general approach to the problem of producing forecasts, or out of sample predictions, from (linear) models containing lagged dependent variables.  Other approaches to producing rolling or iterative forecasts include: \href{https://www.r-bloggers.com/variations-on-rolling-forecasts/}{Variations on rolling forecasts} and \href{https://stackoverflow.com/questions/4856555/iteratively-forecasting-dyn-models}{Iteratively forecasting dyn models}.
-#' @author Dave Mitchell <david.p.mitchell@@homemail.com.au>
+#' @author David Mitchell <david.p.mitchell@@homemail.com.au>
 #' @seealso \code{predict}, \code{predict.lm}, \code{predict.glm}.
 #' @note Function not yet tested against all possible \code{predict} methods.
 #' @examples
@@ -79,6 +194,12 @@ i_predict <- function(object, newdata, ...)
       newdata[.idx, lhsVar] <- .fit[.idx];
     }
     predicted <- predict(object, newdata, ...);
+    ## Temporary check to ensure iterative predictions are updating, and not just producing NAs
+    ##  TO DO: Check rhsVar
+    if ( !all(is.na(predicted) == is.na(predicted0)) )
+      stop(sprintf("Iterative predictions failing -- check dependent variable inputs in %s",
+                   deparse(substitute(newdata))))
+    predicted0 <- predicted
   }
   return(predicted);
 }
@@ -90,7 +211,7 @@ i_predict <- function(object, newdata, ...)
 #' @description Function to return the inverse of the supplied function
 #' @param x valid built-in function character string or mathematical operation.
 #' @return Returns the inverse of the function specified in \code{x} as a character string
-#' @author Dave Mitchell <david.p.mitchell@@homemail.com.au>
+#' @author David Mitchell <david.p.mitchell@@homemail.com.au>
 #' @note Function explicitly defined for use in \code{i_predict}. Function tested against R built-in functions (e.g. log(), sin(), etc.), but not yet thoroughly tested for mathematical expressions (e.g. x'^2' or '2*'x).
 #' @seealso{i_predict}
 #' @examples
--- tests/testthat/test-predict.utils.R
+++ tests/testthat/test-predict.utils.R
@@ -1,28 +1,69 @@
 ## context("predict.utils")
 
-test_that("splice_series works",
+test_that("splice_series.default fails well",
 {
   skip_on_cran()
   skip_on_travis()
   skip_on_appveyor()
 
-  A <- ts(c(100, 95, 125, 150,  NA,  NA,  NA,  NA), start=1991);
-  B <- ts(c(NA,  NA,  NA, 100, 120, 150, 200, 225), start=1991);
+  A <- c(100, 95, 125, 150,  NA,  NA,  NA,  NA);
+  B <- c(NA,  NA,  NA, 100, 120, 150, 200, 225);
 
-  expect_type(splice_series(A, B), typeof(A))
-  # expect_length()
-}
+  expect_error(splice_series(letters[1:8], B, base.period=4))    ## x - non-numeric
+  expect_error(splice_series(A, letters[1:8], base.period=4))    ## y - non-numeric
+  expect_error(splice_series(A, B, base.period=4))               ## base.period of incorrect length
+  expect_error(splice_series(A, B, base.period=rep(4,3)))        ## base.period of incorrect length
+  expect_error(splice_series(A, B, base.period=c(10,3)))         ## base.period x index out of range
+  expect_error(splice_series(A, B, base.period=c(3,10)))         ## base.period y index out of range
+})
 
 
-test_that("splice_series fails well",
+test_that("splice_series works on standard vectors",
 {
   skip_on_cran()
   skip_on_travis()
   skip_on_appveyor()
 
-  # TO DO
-}
+  A <- c(100, 95, 125, 150,  NA,  NA,  NA,  NA);
+  B <- c(NA,  NA,  NA, 100, 120, 150, 200, 225);
 
+  expect_match(typeof(splice_series(A, B, base.period=c(4,4))), typeof(A))
+  expect_match(typeof(splice_series(B, A, base.period=c(4,4), direction="before")), typeof(B))
+  expect_length(splice_series(A, B, base.period=c(4,4)), 8);
+  expect_length(splice_series(B, A, base.period=c(4,4), direction="before"), 8);
+})
+
+
+test_that("splice_series works on time series (ts) objects",
+{
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  A <- ts(c(100, 95, 125, 150,  NA,  NA,  NA,  NA), start=1991, frequency=4);
+  B <- ts(c(NA,  NA,  NA, 100, 120, 150, 200, 225), start=1991, frequency=4);
+
+  expect_match(typeof(splice_series(A, B, base.period=c(1991,4))), typeof(A))
+  expect_length(splice_series(A, B, base.period=c(1991,4)), 8);
+}
+)
+
+
+test_that("splice_series works on extensible time series (xts) objects",
+{
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  A <- xts(x=c(100, 95, 125, 150,  NA,  NA,  NA,  NA),
+              order.by=seq(as.Date("1991-03-01"), by="quarter", length.out=8));
+  B <- xts(x=c(NA,  NA,  NA, 100, 120, 150, 200, 225),
+            order.by=seq(as.Date("1991-03-01"), by="quarter", length.out=8));
+ 
+  expect_match(typeof(splice_series(A, B, base.period=as.Date("1991-12-01"))), typeof(A))
+  expect_equal(nrow(splice_series(A, B, base.period=as.Date("1991-12-01"))), 8);
+}
+)
 
 test_that("i_predict works",
 {
@@ -32,7 +73,7 @@ test_that("i_predict works",
 
   # TO DO
 }
-
+)
 
 test_that("i_predict fails well",
 {
@@ -42,7 +83,7 @@ test_that("i_predict fails well",
 
   # TO DO
 }
-
+)
 
 test_that("inverse_fn works",
 {
@@ -52,7 +93,7 @@ test_that("inverse_fn works",
 
   # TO DO
 }
-
+)
 
 test_that("inverse_fn fails well",
 {
@@ -62,3 +103,4 @@ test_that("inverse_fn fails well",
 
   # TO DO
 }
+)
